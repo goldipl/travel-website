5ea4fdd0d0aed009251ad97936003aae883b5793
goldipl <mgodlewski92@gmail.com>
2023-12-03 13:37:56 +0100

    clean page template

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <h1 className="text-bold text-teal-500">
+    <h1 className="font-bold text-teal-500 text-2xl">
       Hello World
     </h1>
   )
