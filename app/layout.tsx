801bcfc03c5dde25a3ede9d0a5957f95ffeeb121
goldipl <mgodlewski92@gmail.com>
2023-12-03 13:45:03 +0100

    add Footer and Navbar components & include it to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +19,11 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <body>
+        <Navbar />
+        <main className="relative overflow-hidden">
         {children}
+        </main>
+        <Footer />
       </body>
     </html>
   )
