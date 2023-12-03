0d91ab85d27445fb8df8f4be81408ad3c4279176
goldipl <mgodlewski92@gmail.com>
2023-12-03 15:28:52 +0100

    add Button component

---
+++ components/Button.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import React from 'react'
+
+type ButtonProps = {
+    type: 'button' | 'submit';
+    title: string;
+    icon?: string;
+    variant: 'btn_dark_green'
+}
+
+const Button = ({type, title, icon, variant}: ButtonProps) => {
+  return (
+    <button
+        className={`flexCenter gap-3 rounded-full border ${variant}`}
+        type={type}
+    >
+        {icon && <Image src={icon} alt={title} width={24} height={24} className='invert'/>}
+        {title}
+    </button>
+  )
+}
+
+export default Button
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,30 @@
+import { NAV_LINKS } from '@/constants'
 import Link from 'next/link'
 import React from 'react'
+import Button from './Button'
 
 const Navbar = () => {
   return (
     <nav className='flexBetween max-container padding-container relative z-30 py-5'>
-        <Link href="/" className='flex items-center'>
+        <Link href="/" className='flex items-center font-bold'>
             <i className='bx bx-briefcase bx-md'></i>
-            <p className='ml-1 text-lg'>Around<span className='font-bold text-yellow-600'>the</span>World</p>
+            <p className='ml-1 text-lg'>Around<span className='text-yellow-600'>the</span>World</p>
         </Link>
         <ul className='hidden h-full gap-12 lg:flex'>
-            
+            {NAV_LINKS.map((link) => (
+                <Link href={link.href} key={link.key} className='regular-16 text-gray-50 flexCenter cursor-pointer transition-all hover:font-bold'>
+                    {link.label}
+                </Link>
+            ))}
         </ul>
+        <div className='lg:flexCenter hidden'>
+            <Button 
+                type='button'
+                title='Login'
+                icon='./bx-log-in-circle.svg'
+                variant='btn_dark_green'
+            />
+        </div>
     </nav>
   )
 }
