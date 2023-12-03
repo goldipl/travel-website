43e8bce182fcebbd4289439a37e82cdc48199174
goldipl <mgodlewski92@gmail.com>
2023-12-03 14:54:24 +0100

    add array of nav links

---
+++ constants/index.tsx
@@ -0,0 +1,6 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how-we-work', label: 'How We Work?' },
+    { href: '/', key: 'contact', label: 'Contact' },
+];
