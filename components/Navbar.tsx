0cda4d9f4321f06912a02830a65e4041aa37f8fb
goldipl <mgodlewski92@gmail.com>
2023-12-03 14:54:00 +0100

    add some navbar structure & box icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flexBetween max-container padding-container relative z-30 py-5'>
+        <Link href="/" className='flex items-center'>
+            <i className='bx bx-briefcase bx-md'></i>
+            <p className='ml-1 text-lg'>Around<span className='font-bold text-yellow-600'>the</span>World</p>
+        </Link>
+        <ul className='hidden h-full gap-12 lg:flex'>
+            
+        </ul>
+    </nav>
   )
 }
 
