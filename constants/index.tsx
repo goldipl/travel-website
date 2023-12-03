9f41ac019bc29cbbbcdd1935e5eb4532c802c5a7
goldipl <mgodlewski92@gmail.com>
2023-12-03 16:51:51 +0100

    change nav link to about us

--- constants/index.tsx
+++ constants/index.tsx
@@ -1,6 +1,6 @@
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'about-us', label: 'About Us' },
     { href: '/', key: 'how-we-work', label: 'How We Work?' },
     { href: '/', key: 'contact', label: 'Contact' },
 ];
