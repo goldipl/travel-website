dd7712828eaeeda0ff0dced869e1fb0d46bea4ff
goldipl <mgodlewski92@gmail.com>
2023-12-08 15:26:51 +0100

    add gallery to nav links

--- constants/index.tsx
+++ constants/index.tsx
@@ -2,5 +2,6 @@
 export const NAV_LINKS = [
     { href: '/', key: 'about-us', label: 'About Us' },
     { href: '/', key: 'how-we-work', label: 'How We Work?' },
+    { href: '/', key: 'gallery', label: 'Gallery' },
     { href: '/', key: 'contact', label: 'Contact' },
 ];
