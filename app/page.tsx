27d2475651c2b7227bff1cfbc17b07d2e9547859
goldipl <mgodlewski92@gmail.com>
2023-12-10 15:33:19 +0100

    add new componenets AboutUs, HowWeWork, Gallery & Contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import AboutUs from "@/components/AboutUs/AboutUs";
+import Contact from "@/components/Contact/Contact";
+import Gallery from "@/components/Gallery/Gallery";
 import { Hero } from "@/components/Hero/Hero";
+import HowWeWork from "@/components/HowWeWork/HowWeWork";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <AboutUs />
+      <HowWeWork />
+      <Gallery />
+      <Contact />
     </>
   )
 }
---
+++ components/HowWeWork/HowWeWork.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HowWeWork = () => {
+  return (
+    <div>HowWeWork</div>
+  )
+}
+
+export default HowWeWork
