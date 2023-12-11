29a5ca945fee799c735af3b8b7a9897b31e165d7
goldipl <mgodlewski92@gmail.com>
2023-12-11 16:59:27 +0100

    restyling margins

--- components/HowWeWork/HowWeWork.tsx
+++ components/HowWeWork/HowWeWork.tsx
@@ -5,8 +5,8 @@ const HowWeWork = () => {
   return (
     <div className='max-container padding-container py-10'>
       <div className="bg-cover bg-no-repeat bg-center p-8 rounded-3xl" style={{ backgroundImage: `url(${sandImage.src})` }}>
-        <h1 className='flex flexCenter bg-opacity-50 bg-white p-8 rounded-3xl text-black font-bold m-10 regular-24 xl:regular-40'>How We Work</h1>
-        <p className='flex flexCenter bg-opacity-50 bg-white p-8 rounded-3xl text-black m-10'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, minus. Natus iusto, alias nemo nobis exercitationem et, assumenda molestias illum sunt laudantium corrupti ut accusamus, debitis rem! Maxime, minima. Dolores!
+        <h1 className='flex flexCenter bg-opacity-50 bg-white p-8 rounded-3xl text-black font-bold mb-4 lg:m-10 regular-24 xl:regular-40'>How We Work</h1>
+        <p className='flex flexCenter bg-opacity-50 bg-white p-8 rounded-3xl text-black mb-4 lg:m-10'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, minus. Natus iusto, alias nemo nobis exercitationem et, assumenda molestias illum sunt laudantium corrupti ut accusamus, debitis rem! Maxime, minima. Dolores!
         Quaerat culpa eos similique, illo, libero soluta quo ab nostrum non natus rem? Saepe sapiente debitis perferendis, distinctio voluptas facilis maiores harum culpa ea, illum dolores aliquam sunt voluptate. Qui!
         Voluptatibus, sit illo sunt doloribus iure nulla dolores, a enim consequatur obcaecati eveniet corrupti, tenetur iusto culpa adipisci facilis quos dignissimos temporibus numquam rerum ab fugiat accusantium? Quisquam, illum cum.
         Omnis minima tempore alias deleniti dignissimos velit numquam amet eveniet ratione assumenda, quia ea eligendi exercitationem tempora, eos dolores cumque, rerum officiis incidunt asperiores? Voluptatibus dolor odio saepe aut maxime!
