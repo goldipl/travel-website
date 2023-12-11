a7d383f61a8adecbdf2ed0a2e74e93986a18ab0d
goldipl <mgodlewski92@gmail.com>
2023-12-11 16:31:25 +0100

    delete unused themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,33 +19,10 @@ module.exports = {
           50: '#585858',
           90: '#141414',
         },
-        orange: {
-          50: '#FF814C',
-        },
-        blue: {
-          70: '#021639',
-        },
-        yellow: {
-          50: '#FEC601',
-        },
-      },
-      backgroundImage: {
-        'bg-img-1': "url('/img-1.png')",
-        'bg-img-2': "url('/img-2.png')",
-        'feature-bg': "url('/feature-bg.png')",
-        pattern: "url('/pattern.png')",
-        'pattern-2': "url('/pattern-bg.png')",
       },
       screens: {
-        xs: '400px',
         '3xl': '1680px',
         '4xl': '2200px',
-      },
-      maxWidth: {
-        '10xl': '1512px',
-      },
-      borderRadius: {
-        '5xl': '40px',
       },
     },
   },
