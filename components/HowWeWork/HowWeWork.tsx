a2dcdd4a846e01048e70059e75fb3d35e117c302
goldipl <mgodlewski92@gmail.com>
2023-12-11 17:02:07 +0100

    correct marings on last paragraph

--- components/HowWeWork/HowWeWork.tsx
+++ components/HowWeWork/HowWeWork.tsx
@@ -11,7 +11,7 @@ const HowWeWork = () => {
         Voluptatibus, sit illo sunt doloribus iure nulla dolores, a enim consequatur obcaecati eveniet corrupti, tenetur iusto culpa adipisci facilis quos dignissimos temporibus numquam rerum ab fugiat accusantium? Quisquam, illum cum.
         Omnis minima tempore alias deleniti dignissimos velit numquam amet eveniet ratione assumenda, quia ea eligendi exercitationem tempora, eos dolores cumque, rerum officiis incidunt asperiores? Voluptatibus dolor odio saepe aut maxime!
         Debitis ea placeat id, iste, inventore repellendus veritatis vel fuga consequuntur ullam consequatur a enim quis laudantium alias iure libero tempore sapiente reprehenderit ratione quam quasi nemo laboriosam. Animi, corporis.</p>
-        <p className='flex flexCenter bg-opacity-50 bg-white p-8 rounded-3xl text-black m-10'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, minus. Natus iusto, alias nemo nobis exercitationem et, assumenda molestias illum sunt laudantium corrupti ut accusamus, debitis rem! Maxime, minima. Dolores!
+        <p className='flex flexCenter bg-opacity-50 bg-white p-8 rounded-3xl text-black mb-4 lg:m-10'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, minus. Natus iusto, alias nemo nobis exercitationem et, assumenda molestias illum sunt laudantium corrupti ut accusamus, debitis rem! Maxime, minima. Dolores!
         Debitis ea placeat id, iste, inventore repellendus veritatis vel fuga consequuntur ullam consequatur a enim quis laudantium alias iure libero tempore sapiente reprehenderit ratione quam quasi nemo laboriosam. Animi, corporis.</p>
       </div>
     </div>
