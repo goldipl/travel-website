c6ade4039cd2207540ff77c851f8a98253b8e9d8
goldipl <mgodlewski92@gmail.com>
2023-12-12 17:22:00 +0100

    add overflow:hidden to Gallery component

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -4,7 +4,7 @@ import Swiper from './Swiper'
 
 const Gallery = () => {
   return (
-    <div className='max-container padding-container py-10'>
+    <div className='max-container padding-container py-10 overflow-hidden'>
       <Swiper />
     </div>
   )
