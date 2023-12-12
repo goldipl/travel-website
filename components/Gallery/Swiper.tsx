a45b0800ab73bb12c1e7d8ef41aa9800ae8540d3
goldipl <mgodlewski92@gmail.com>
2023-12-12 17:20:41 +0100

    set swiper width 100vw

--- components/Gallery/Swiper.tsx
+++ components/Gallery/Swiper.tsx
@@ -19,30 +19,31 @@ import img08 from '@/public/images/swiper/swiper-image08.jpg'
 export default () => {
   return (
     <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      breakpoints={{
-        // when window width is >= 320px
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        // when window width is >= 800px
-        800: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        // when window width is >= 1200px
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-        },
-      }}
+        className='w-screen'
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        breakpoints={{
+            // when window width is >= 320px
+            320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            },
+            // when window width is >= 800px
+            800: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            },
+            // when window width is >= 1200px
+            1200: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+            },
+        }}
     >
         <SwiperSlide>
             <img src={img01.src} alt="img" className='select-none rounded-xl'/>
