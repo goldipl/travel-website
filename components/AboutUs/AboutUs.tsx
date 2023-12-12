ac469df79045d44e55695826ddae097b89e7b755
goldipl <mgodlewski92@gmail.com>
2023-12-12 17:50:44 +0100

    restyling imgs top position on 2xl screens

--- components/AboutUs/AboutUs.tsx
+++ components/AboutUs/AboutUs.tsx
@@ -6,10 +6,10 @@ const AboutUs = () => {
   return (
     <div id='about_us' className='relative flex max-container padding-container items-center py-10'>
       <div className='flex h-[400px]'>
-        <div className='flex absolute left-[-80px] top-[-16px] lg:top-[-20px] md:top-[-80px] md:left-0'>
+        <div className='flex absolute left-[-80px] top-[-16px] 2xl:top-[-60px] lg:top-[-20px] md:top-[-80px] md:left-0'>
           <img className='rounded-3xl border-2 border-white' src={MaledviesImg01.src} alt="img" width={400} height={1200}/>
         </div>
-        <div className='flex absolute z-[-1] left-1/3 right-[-40px] top-[80px] h-5/6 lg:top-[-60px] lg:h-[120%] lg:z-0'>
+        <div className='flex absolute z-[-1] left-1/3 right-[-40px] top-[80px] h-5/6 2xl:top-[-120px] lg:top-[-60px] lg:h-[120%] lg:z-0'>
           <img className='rounded-3xl border-2 border-white' src={MaledviesImg02.src} alt="img" width={400} height={1200}/>
         </div>
       </div>
