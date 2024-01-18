8057b416b7b10e26a6b9a54326aa09eaf8dfc99e
goldipl <mgodlewski92@gmail.com>
2024-01-18 16:59:23 +0100

    add nav links

--- constants/index.tsx
+++ constants/index.tsx
@@ -1,7 +1,7 @@
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'about-us', label: 'About Us' },
-    { href: '/', key: 'how-we-work', label: 'How We Work?' },
-    { href: '/', key: 'gallery', label: 'Gallery' },
-    { href: '/', key: 'contact', label: 'Contact' },
+    { href: '#about_us', key: 'about-us', label: 'About Us' },
+    { href: '#how_we_work', key: 'how-we-work', label: 'How We Work?' },
+    { href: '#gallery', key: 'gallery', label: 'Gallery' },
+    { href: '#contact', key: 'contact', label: 'Contact' },
 ];
