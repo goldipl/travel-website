567ab3e85dd5716a5d2d8ee5cd4746238a422559
goldipl <mgodlewski92@gmail.com>
2024-01-18 16:55:13 +0100

    add fotter content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='max-container padding-container py-10'>
-      Footer
+      Travel Website 2024
     </div>
   )
 }
