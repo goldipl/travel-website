f1cb1a06803f6bac0297069dd97018906a26e2b5
goldipl <mgodlewski92@gmail.com>
2024-02-10 16:21:40 +0100

    styling footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='max-container padding-container py-10'>
-      Travel Website 2024
+    <div className="bg-black text-white padding-container py-10 w-[100vw] flex justify-center lg:justify-between">
+      <div className="max-container flex items-center lg:items-start w-full lg:flex-row flex-col justify-center lg:justify-between">
+        <div className="flex-col">
+          <a href="/"className="flex justify-center">
+            <img
+              className="lg:w-[120px] h-auto w-[60px] m-6"
+              src="/_next/static/media/logo.5c571377.png"
+              alt="logo"
+              width="150"
+              height="143"
+            ></img>
+          </a>
+          <p>© 2024 Travel Website</p>
+        </div>
+        <div className="flex-col mb-4">
+          <h3 className="uppercase text-lg mb-4">Navigation</h3>
+          <ul>
+            <li className="text-center">
+              <a className="text-sm" href="#about_us">
+                About Us
+              </a>
+            </li>
+            <li className="text-center">
+              <a className="text-sm" href="#how_we_work">
+                How We Work?
+              </a>
+            </li>
+            <li className="text-center">
+              <a className="text-sm" href="#gallery">
+                Gallery
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-col mb-4">
+          <h3 className="uppercase text-lg mb-4">Discounts</h3>
+          <ul>
+            <li className="text-center">
+              <a className="text-sm" href="/">
+                Read more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-col text-center">
+          <h3 className="uppercase text-lg mb-4">Social Media</h3>
+            <a href="#">
+              <i className='bx bxl-facebook-square mx-1'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-instagram mx-1'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-linkedin-square mx-1'></i>
+            </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
