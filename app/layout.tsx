99683ed44ec267256f3d5d28ecc78a21b20bfc5b
goldipl <mgodlewski92@gmail.com>
2024-02-10 16:30:31 +0100

    add styles & styling swiper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import './../components/Gallery/gallery.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
