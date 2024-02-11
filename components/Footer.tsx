82f0ac36d12b61686f7110be50cc3f6ac573d5c8
goldipl <mgodlewski92@gmail.com>
2024-02-11 10:06:48 +0100

    add hover to footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,27 +21,27 @@ const Footer = () => {
           <h3 className="uppercase text-lg mb-4">Navigation</h3>
           <ul>
             <li className="text-center">
-              <a className="text-sm" href="#about_us">
+              <a className="text-sm hover:text-[#00df9a]" href="#about_us">
                 About Us
               </a>
             </li>
             <li className="text-center">
-              <a className="text-sm" href="#how_we_work">
+              <a className="text-sm hover:text-[#00df9a]" href="#how_we_work">
                 How We Work?
               </a>
             </li>
             <li className="text-center">
-              <a className="text-sm" href="#gallery">
+              <a className="text-sm hover:text-[#00df9a]" href="#gallery">
                 Gallery
               </a>
             </li>
           </ul>
         </div>
         <div className="flex-col mb-4">
-          <h3 className="uppercase text-lg mb-4">Discounts</h3>
+          <h3 className="uppercase text-lg mb-4">Pricing</h3>
           <ul>
             <li className="text-center">
-              <a className="text-sm" href="/">
+              <a className="text-sm hover:text-[#00df9a]" href="/">
                 Read more
               </a>
             </li>
@@ -50,13 +50,13 @@ const Footer = () => {
         <div className="flex-col text-center">
           <h3 className="uppercase text-lg mb-4">Social Media</h3>
             <a href="#">
-              <i className='bx bxl-facebook-square mx-1'></i>
+              <i className='bx bxl-facebook-square mx-1 hover:text-[#00df9a]'></i>
             </a>
             <a href="#">
-              <i className='bx bxl-instagram mx-1'></i>
+              <i className='bx bxl-instagram mx-1 hover:text-[#00df9a]'></i>
             </a>
             <a href="#">
-              <i className='bx bxl-linkedin-square mx-1'></i>
+              <i className='bx bxl-linkedin-square mx-1 hover:text-[#00df9a]'></i>
             </a>
         </div>
       </div>
