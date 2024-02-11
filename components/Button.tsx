6d8923020d8334b44226e34c317c24c31881293b
goldipl <mgodlewski92@gmail.com>
2024-02-11 10:27:05 +0100

    add another option "width" to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,15 +6,16 @@ type ButtonProps = {
     title: string;
     icon?: string;
     variant: 'btn_light_lime'
+    width: string;
 }
 
-const Button = ({type, title, icon, variant}: ButtonProps) => {
+const Button = ({type, title, icon, variant, width}: ButtonProps) => {
   return (
     <button
-        className={`flexCenter gap-3 rounded-full border ${variant}`}
+        className={`duration-150 rounded-md font-medium my-6 mx-auto px-6 py-3 ${variant} w-[${width}px]`}
         type={type}
     >
-        {icon && <Image src={icon} alt={title} width={24} height={24} className='invert'/>}
+        {icon && <Image src={icon} alt={title} width={22} height={22} className='inline-flex mr-3'/>}
         {title}
     </button>
   )
